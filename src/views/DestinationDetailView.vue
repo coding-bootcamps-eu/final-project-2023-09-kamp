ff1c838945d5fa9297f592f69c9f7038c7935fda
Andreas Ullrich <ullrich.andi@web.de>
2023-12-31 13:36:23 +0100

    fix: link and show connections

--- src/views/DestinationDetailView.vue
+++ src/views/DestinationDetailView.vue
@@ -1,53 +1,85 @@
 <template>
-  <header>
-    <article>
-      <h1 v-for="region in regions" :key="region.id">{{ region.name }}</h1>
-    </article>
-  </header>
-  <main>
-    <article v-for="destination in destinations" :key="destination.id">
-      <h2>{{ destination.name }}</h2>
-      <div class="img-container">
-        <img :src="destination.imgSrc" alt="Bild" />
-      </div>
-      <p>
-        <small>{{ destination.category }}</small>
-      </p>
-      <p class="text-container">{{ destination.description }}</p>
-      <section>
-        <h2>Öffnungszeiten</h2>
-        <p>{{ destination.openingTime }} - {{ destination.closingTime }}</p>
-      </section>
-      <section>
-        <h2>Kosten</h2>
-        <p>{{ destination.price }}</p>
-      </section>
-      <section>
-        <h2>Ort</h2>
-        <div class="map-outer-container">
-          <div class="map-container" ref="map"></div>
+  <div>
+    <h1>{{ destination }}</h1>
+    <main>
+      <article>
+        <div class="img-container">
+          <img :src="getImgSrc(destination)" :alt="destination.altText" class="img" />
         </div>
-      </section>
-    </article>
-  </main>
+        <p>
+          <small>{{ $route }}</small>
+        </p>
+        <p class="text-container">{{ destination.description }}</p>
+        <section>
+          <h2>Öffnungszeiten</h2>
+          <p>{{ destination.openingTime }} - {{ destination.closingTime }}</p>
+        </section>
+        <section>
+          <h2>Kosten</h2>
+          <p>{{ destination.price }}</p>
+        </section>
+        <section>
+          <h2>Ort</h2>
+          <div class="map-outer-container">
+            <div class="map-container" ref="map"></div>
+          </div>
+        </section>
+      </article>
+    </main>
+  </div>
 </template>
 
 <script>
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
-//import NavBar from "./components/Navbar.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    //NavBar,
-  },
   data() {
     return {
-      regions: [],
-      destinations: []
+      destination: {}
     }
   },
+  async created() {
+    const response = await fetch('http://localhost:8080/destinations' + this.route.params.id)
+    this.destination = await response.json()
+  },
+
+  methods: {
+    getImgSrc(destination) {
+      if (destination.imgSrc) {
+        return `http://localhost:3333/${destination.imgSrc}`
+      } else {
+        return 'http://localhost:3333/img/destinations/ChineseGardenMunich.jpg'
+      }
+    }
+
+    /*
+    initMap(coordinates) {
+      this.map = L.map(this.$refs.map).setView(coordinates, 13)
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(this.map)
+
+      L.marker(coordinates).addTo(this.map).bindPopup(this.destination.name).openPopup()
+    }*/
+  },
+  /*
+  mounted() {
+    this.fetchDestination()
+
+    this.map = L.map(this.$refs.map).setView([51.505, -0.09], 13)
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map)
+
+    this.destinations.forEach((destination) => {
+      if (destination.coordinates) {
+        L.marker(destination.coordinates).addTo(this.map).bindPopup(destination.name).openPopup()
+      }
+    })
+  }*/
   mounted() {
     this.map = L.map(this.$refs.map).setView([51.505, -0.09], 13)
 
@@ -63,13 +95,6 @@ export default {
     destinations.forEach((destination) => {
       L.marker(destination.coordinates).addTo(this.map).bindPopup(destination.name).openPopup()
     })
-  },
-  created() {
-    fetch('http://localhost:8080/destinations')
-      .then((response) => response.json())
-      .then((jsonData) => {
-        this.destinations = jsonData
-      })
   }
 }
 </script>
