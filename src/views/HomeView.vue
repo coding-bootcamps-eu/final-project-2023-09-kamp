cfdfb3d97e67abf4f5242be74a54c811a4736e48
czeopauli <czepulonis.pauli@gmail.com>
2024-01-08 16:20:55 +0100

    feat: added some styling to homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <template>
-  <main class="content">
+  <main
+    class="content"
+    :style="{ backgroundImage: 'url(https://i.imgur.com/xfx7DFh.jpg)' }"
+    alt="Schönes Bergpanorama mit einem Pfad, der sich hinauf zu den Bergen schlängelt"
+  >
     <div class="header">
       <h1>KAMP</h1>
       <p>Dein Begleiter für barrierefreies Abenteuer</p>
     </div>
+    <div class="center-content"></div>
     <div class="bottom section">
       <select v-model="MainStore.selectedRegionId" class="select-dropdown">
         <option disabled selected value="" hidden>Bitte Region auswählen</option>
@@ -54,59 +59,70 @@ export default {
   box-sizing: border-box;
 }
 
-.content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
+@media (max-width: 600px) {
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-evenly;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+  }
 
-.header {
-  background: var(--background-color);
-  margin: 2rem;
-  padding: 16px;
-  border-radius: 20px;
-}
+  .header {
+    background: var(--background-color);
+    margin: 2rem;
+    padding: 16px;
+    border-radius: 20px;
+  }
 
-h1 {
-  color: var(--highlight-color);
-  font-size: 3.5rem;
-  margin-bottom: 8px;
-}
+  h1 {
+    color: var(--highlight-color);
+    font-size: 3.5rem;
+    margin-bottom: 8px;
+  }
 
-p {
-  color: white;
-  margin-bottom: 0;
-}
+  p {
+    color: var(--btn-text-color);
+    margin-bottom: 0;
+  }
 
-.section {
-  margin-top: 20px;
-  text-align: center;
-}
+  .section {
+    margin-top: 20px;
+    text-align: center;
+  }
 
-.select-dropdown {
-  width: 80%;
-  padding: 10px;
-  border: 1px solid var(--text-color);
-  border-radius: 5px;
-  margin-bottom: 10px;
-  font-family: var(--main-font);
-}
+  .select-dropdown {
+    background-color: var(--background-color);
+    color: var(--highlight-color);
+    text-align: center;
+    font-weight: bold;
+    width: 80%;
+    padding: 10px;
+    border: 1px solid var(--text-color);
+    border-radius: 5px;
+    margin-bottom: 20px;
+    font-family: var(--main-font);
+  }
 
-.start-button {
-  background-color: var(--btn-color);
-  color: var(--btn-text-color);
-  padding: 10px;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-  font-family: var(--main-font);
-}
+  .start-button {
+    background-color: var(--background-color);
+    color: var(--btn-text-color);
+    font-weight: bold;
+    width: 40%;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-family: var(--main-font);
+  }
 
-.warning {
-  color: red;
-  margin-top: 10px;
-  text-align: center;
-  font-family: var(--main-font);
+  .warning {
+    color: red;
+    margin-top: 10px;
+    text-align: center;
+    font-family: var(--main-font);
+  }
 }
 </style>
