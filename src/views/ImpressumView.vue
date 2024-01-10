1e886d20dd7e881ccbcf5a4864d007764e1f6fe4
czeopauli <czepulonis.pauli@gmail.com>
2024-01-10 15:20:30 +0100

    fix: resolve import error in ImpressumVue

---
+++ src/views/ImpressumView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <navbar />
+    <div class="impressum-container">
+      <h2>Impressum</h2>
+      <p>KAMP</p>
+      <p>Musterstraße 123</p>
+      <p>12345 Musterstadt</p>
+      <p>Telefon: 01234 567890</p>
+      <p>E-Mail: info@kamp.de</p>
+      <p>Umsatzsteuer-ID: DE123456789</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useMainStore } from '@/stores/mainStore'
+import NavBar from '@/components/NavBar.vue'
+
+export default {
+  data() {
+    const mainStore = useMainStore()
+    return { mainStore }
+  },
+  components: {
+    navbar: NavBar
+  }
+}
+</script>
+
+<style scoped></style>
